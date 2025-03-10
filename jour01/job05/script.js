a3bf51db71890a7bbe8375393661a4562cd8226d
Antoine <antoine.leca@laplateforme.io>
2025-03-10 13:01:18 +0100

    ajout du jour01/job05 fini

---
+++ jour01/job05/script.js
@@ -0,0 +1,9 @@
+function AfficherJoursSemaine() {
+    let str = ["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]
+
+    for (i=0; i < 7; i++) {
+        console.log(str[i]);
+    }
+}
+
+AfficherJoursSemaine();
